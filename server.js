afc70ed50d9fce3ec3f486a4fb11766dc765cabd
Gregor Wolf <gregor.wolf@gmail.com>
2020-02-28 12:31:28 +0100

    Return two entries

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ var users =
 {
     "users": [
         {
+
+            "metadata": {
+                "guid": "d7448ab8-3a25-4263-9010-c7bf46ddbc28",
+                "url": "https://hxehost:30030/v2/users/d7448ab8-3a25-4263-9010-c7bf46ddbc28",
+                "created_at": "1563827457574",
+                "updated_at": "1571133286714"
+            },
+            "userEntity": {
+                "uaaGuid": "157715",
+                "username": "XSA_ADMIN",
+                "origin": "",
+                "active": true
+            }
+        
+        },
+        {
             "metadata": {
                 "guid": "fb8a3f99-6d74-4569-8b0a-4f9ecb43cb2c",
                 "url": "https://hxehost:30030/v2/users/fb8a3f99-6d74-4569-8b0a-4f9ecb43cb2c",
